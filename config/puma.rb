c256fd92cd75c74ae3353db071b6180c68665e7b
shimabee <mrs1122ani@gmail.com>
2020-03-25 08:59:19 +0900

    pumaの設定

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -32,3 +32,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# socketの設定
+# ディレクトリがない場合は作成しておく
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
